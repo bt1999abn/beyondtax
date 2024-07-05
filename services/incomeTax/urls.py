4fb9703e50f9b608f338ae3678f227b81333cb0f
bhargavnaidu2806 <sai28061999@gmail.com>
2024-07-05 16:21:56 +0530

    abn_income_tax_residential_status_api_push

--- services/incomeTax/urls.py
+++ services/incomeTax/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IncomeTaxProfileApi, ListIncomeTaxReturnsView
+from .views import IncomeTaxProfileApi, ListIncomeTaxReturnsView, ResidentialStatusQuestionsListView
 
 urlpatterns = [
     path('create-incometax-profile/', IncomeTaxProfileApi.as_view(), name='create-incometax-profile'),
     path('update-incometax-profile/', IncomeTaxProfileApi.as_view(), name='update-incometax-profile'),
     path('retrive-incometax-profile/', IncomeTaxProfileApi.as_view(), name='retrive-incometax-profile'),
     path('income-tax-returns/', ListIncomeTaxReturnsView.as_view(), name='user-income-tax-returns'),
+    path('residential-status-questions/', ResidentialStatusQuestionsListView.as_view(), name='residential-status-questions'),
 ]
