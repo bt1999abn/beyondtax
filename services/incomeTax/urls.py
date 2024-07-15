13f430a3eafc4699fd022bcb43926f0374ff1128
bhargav naidu <bt1999abn@gmail.com>
2024-07-15 10:55:49 -0700

    abn_income_tax_dashboard_push

--- services/incomeTax/urls.py
+++ services/incomeTax/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import IncomeTaxProfileApi, ListIncomeTaxReturnsView, ResidentialStatusQuestionsListView
+from .views import IncomeTaxProfileApi, ListIncomeTaxReturnsView, ResidentialStatusQuestionsListView, \
+    SendPanVerificationOtpApi, VerifyPanOtpApi, ImportIncomeTaxProfileDataApi
 
 urlpatterns = [
     path('create-incometax-profile/', IncomeTaxProfileApi.as_view(), name='create-incometax-profile'),
@@ -7,4 +8,7 @@ urlpatterns = [
     path('retrive-incometax-profile/', IncomeTaxProfileApi.as_view(), name='retrive-incometax-profile'),
     path('income-tax-returns/', ListIncomeTaxReturnsView.as_view(), name='user-income-tax-returns'),
     path('residential-status-questions/', ResidentialStatusQuestionsListView.as_view(), name='residential-status-questions'),
+    path('send-pan-verification-otp/', SendPanVerificationOtpApi.as_view(), name='send_pan_verification_otp'),
+    path('verify-pan-otp/', VerifyPanOtpApi.as_view(), name='verify_pan_otp'),
+    path('import-tax-profile-data/', ImportIncomeTaxProfileDataApi.as_view(), name='import_tax_profile_data'),
 ]
